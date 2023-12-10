2520c524b49def4340b3339e2d264bb02e04e9da
Olha Kozaryk <114346084+Olga-kozaryk@users.noreply.github.com>
2023-12-10 18:09:38 +0200

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,13 @@
+import { Component } from "react";
+
+
+
+
+
+
+
+
+
 export const App = () => {
   return (
     <div
@@ -10,7 +20,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+     
     </div>
   );
 };
