25bfd1b6abbd1797878818fa467a60dcd451cc89
Olha Kozaryk <114346084+Olga-kozaryk@users.noreply.github.com>
2023-12-11 20:41:25 +0200

    app

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,13 @@
+import { StatisticsList } from "./StatisticsList.styled";
+
 export const Statistics = ({good, neutral, bad, total, positivePercentage}) => {
     return (
-        <ul>
+        <StatisticsList>
             <li>Good: {good}</li>
             <li>Neutral: {neutral}</li>
             <li>Bad: {bad}</li>
             <li>Total: {total}</li>
             <li>PositivePercentage: {positivePercentage} %</li>
-        </ul>
+        </StatisticsList>
     );
 };
